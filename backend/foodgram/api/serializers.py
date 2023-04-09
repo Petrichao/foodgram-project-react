3e8caa9ddaffc31aa1f00f6de39ddca7d2d203e0
Egor Petrov <egor29302@yandex.ru>
2023-04-09 16:58:02 +0300

    Post request recipe

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,9 +1,22 @@
+import base64
+
+from django.core.files.base import ContentFile
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
 from recipes import models as r_models
 
 User = get_user_model()
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -16,6 +29,11 @@ class AuthorSerializer(serializers.ModelSerializer):
             'last_name',
         )
         model = User
+
+
+class IngredientsPostSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    amount = serializers.FloatField()
 
 
 class TagsSerializer(serializers.ModelSerializer):
@@ -60,10 +78,12 @@ class RecipesSerializer(serializers.ModelSerializer):
     ingredients = IngredientAmountSerializer(many=True)
     author = AuthorSerializer()
     is_favorited = serializers.SerializerMethodField()
+    image = Base64ImageField()
 
     class Meta:
         model = r_models.Recipes
         exclude = ('pub_date',)
+        read_only_fields = ('author',)
 
     def get_is_favorited(self, obj):
         request = self.context.get('request')
@@ -71,3 +91,40 @@ class RecipesSerializer(serializers.ModelSerializer):
         recipe_id = obj.id
         return r_models.RecipeFavorited.objects.filter(user=user_id,
                                                        recipe=recipe_id).exists()
+
+
+class RecipesPostSerializer(serializers.ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=r_models.Tags.objects.all())
+    ingredients = IngredientsPostSerializer(many=True)
+    image = Base64ImageField()
+
+    class Meta:
+        model = r_models.Recipes
+        exclude = ('pub_date',)
+        read_only_fields = ('author',)
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        recipe = r_models.Recipes.objects.create(**validated_data)
+
+        for ingredient in ingredients:
+            ingredient_id = ingredient.get('id')
+            current_ingredient = get_object_or_404(r_models.Ingredients, pk=ingredient_id)
+            ingam = r_models.IngredientsAmount.objects.create(
+                ingredient=current_ingredient,
+                amount=ingredient.get('amount')
+            )
+            ingam.save()
+            r_models.IngredientsRecipe.objects.create(
+                ingredient_amount=ingam,
+                recipe=recipe
+            )
+
+        for tag in tags:
+            r_models.TagsRecipe.objects.create(
+                tag=tag,
+                recipe=recipe
+            )
+        return recipe
+
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -25,3 +25,15 @@ class IngredientsViewSet(GetViewsets):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = models.Recipes.objects.all()
     serializer_class = serializers.RecipesSerializer
+
+    def get_serializer_class(self):
+        if self.request.method in ("POST", "PATCH"):
+            return serializers.RecipesPostSerializer
+        return serializers.RecipesSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+
+
